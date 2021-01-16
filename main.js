a5ee0d8dba4c9308fd27479d44be84d7480100dd
William Kim <william.kim171@gmail.com>
2021-01-16 18:11:24 +1100

    Second Commit

--- main.js
+++ main.js
@@ -1,21 +1,61 @@
 function initMap() {
-      const uluru = { lat: -25.344, lng: 131.036 };
-      const map = new google.maps.Map(document.getElementById("map"), {
-         zoom: 6,
-         center: uluru,
+   const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 6,
+      center: { lat: -33, lng: 151 },
+   });
+/*
+   const marker = new google.maps.Marker({
+      position: { lat: -33.89, lng: 151.274 },
+      map,
+      icon:"https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png"
+   });
+
+   const infoWindow = new google.maps.InfoWindow({
+      content:'<h1>Bondi</h1>'
+   });
+
+   marker.addListener('click', function() {
+      infoWindow.open(map, marker);
+   });
+*/
+   var markers = [
+      {
+         coords:{lat: -33.89, lng: 151.274},
+         iconImage:"https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png",
+         content:'<h1>Bondi</h1>'
+      },
+      {
+         coords:{lat: -37.8136, lng: 144.9631}
+      }
+   ];
+
+   for (var i = 0; i<markers.length ; i++){
+      addMarker(markers[i]);
+   }
+
+   function addMarker(props){
+      const marker = new google.maps.Marker({
+         position: props.coords,
+         map: map,
+         icon:props.iconImage
       });
-      // const marker = new google.maps.Marker({
-      //    position: uluru,
-      //    map: map,
-      // });
+
+      if(props.iconImage){
+         marker.setIcon(props.iconImage);
+      }
+
+      if(props.content){
+         const infoWindow = new google.maps.InfoWindow({
+            content:props.content
+         });
+      
+         marker.addListener('click', function() {
+            infoWindow.open(map, marker);
+         });
+      }
+   }
 }
 
-function addMarker(coords){
-      const marker = new google.maps.Marker({
-         position: coords,
-         map:map,
-         icon:"https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png"
-      });
-}
-addMarker({ lat: -25.344, lng: 131.036 });
+
+
 
